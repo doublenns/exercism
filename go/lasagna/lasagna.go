1fcff76bf9aa8479ec1acff9b1b1d25b33388d63
doublenns <nnamdi.anyanwu@gmail.com>
2021-09-03 22:53:40 -0500

    Overlooked simplification of ElaspedTime

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -7,5 +7,5 @@ func RemainingOvenTime(t int) int { return OvenTime() - t }
 func PreparationTime(l int) int { return l * 2 }
 
 func ElapsedTime(l, t int) int {
-	return PreparationTime(l) + (OvenTime() - RemainingOvenTime(t))
+	return PreparationTime(l) + t
 }
