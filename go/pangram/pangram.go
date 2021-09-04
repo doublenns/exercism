145f740adf23149ed07bc57ff688246cefd93d13
doublenns <nnamdi.anyanwu@gmail.com>
2021-09-03 21:11:01 -0500

    Pangram solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,21 +1,28 @@
 package pangram
 
-func removeNonAlphas(s string) string {
+import (
+	"strings"
+)
+
+func removeNonLowerAlphas(s string) string {
 	var result strings.Builder
 	for i := 0; i < len(s); i++ {
 		b := s[i]
-		if ('a' <= b && b <= 'z'){
+		if 'a' <= b && b <= 'z' {
 			result.WriteByte(b)
 		}
 	}
 	return result.String()
 }
 
-
 func IsPangram(s string) bool {
 	letters := make(map[rune]bool)
-	
-	for _, r range := s {
 
+	for _, r := range removeNonLowerAlphas(strings.ToLower(s)) {
+		letters[r] = true
 	}
+	if len(letters) == 26 {
+		return true
+	}
+	return false
 }
