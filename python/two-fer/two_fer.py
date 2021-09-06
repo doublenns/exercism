9870b491a82eec2139e2c8de3065db1eb86968b4
doublenns <nnamdi.anyanwu@gmail.com>
2021-09-06 00:48:26 -0500

    Add doc string comments

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,3 +1,9 @@
+'''
+this module  implements a two-fer sentence.
+'''
 def two_fer(name="you"):
+    '''
+    two-fer or 2-fer is short for two for one: "One for you and one for me."
+    Given a name, this function returns a two-fer message string.
+    '''
     return f"One for {name}, one for me."
-    
