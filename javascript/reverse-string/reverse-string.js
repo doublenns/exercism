8acf2432df7370938ce1b7b490f824717ddee5ac
doublenns <nnamdi.anyanwu@gmail.com>
2021-09-17 19:15:45 -0400

    Reverse string using array destructuring

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -3,12 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-import { join } from "path/posix";
+// import { join } from "path/posix";
 
 export const reverseString = (str) => {
   // Built-in functions
+  // let splitString = str.split("");
+  // let reversedArray = splitString.reverse();
+  // let joinedArray = reversedArray.join("");
+  // return joinedArray;
+
+  // Multiple assignment using array destructuring
+  let [i, j] = [0, str.length];
   let splitString = str.split("");
-  let reversedArray = splitString.reverse();
-  let joinedArray = reversedArray.join("");
-  return joinedArray;
-};
+  while (i < j) {
+    [splitString[i], splitString[j]] = [splitString[j], splitString[i]];
+    i++;
+    j--;
+  }
+  return splitString.join("");
+}
