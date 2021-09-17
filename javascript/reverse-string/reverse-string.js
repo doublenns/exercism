a96da7d92d7bd7b0a96565af223d897530b6d045
doublenns <nnamdi.anyanwu@gmail.com>
2021-09-17 19:19:02 -0400

    Retest first solution, with import and comments removed

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,24 +1,17 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// import { join } from "path/posix";
-
 export const reverseString = (str) => {
   // Built-in functions
-  // let splitString = str.split("");
-  // let reversedArray = splitString.reverse();
-  // let joinedArray = reversedArray.join("");
-  // return joinedArray;
+  let splitString = str.split("");
+  let reversedArray = splitString.reverse();
+  let joinedArray = reversedArray.join("");
+  return joinedArray;
 
   // Multiple assignment using array destructuring
-  let [i, j] = [0, str.length];
-  let splitString = str.split("");
-  while (i < j) {
-    [splitString[i], splitString[j]] = [splitString[j], splitString[i]];
-    i++;
-    j--;
-  }
-  return splitString.join("");
+  // let [i, j] = [0, str.length];
+  // let splitString = str.split("");
+  // while (i < j) {
+  //   [splitString[i], splitString[j]] = [splitString[j], splitString[i]];
+  //   i++;
+  //   j--;
+  // }
+  // return splitString.join("");
 }
