658cc9b26262d16784007dd19f6c6cc99cb3e21f
doublenns <nnamdi.anyanwu@gmail.com>
2021-09-19 13:03:36 -0400

    Check whether February has 29 days that year

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,6 @@
-export function isLeap(year: number): boolean {
-  return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+export function isLeap(year: number = new Date().getFullYear()): boolean {
+  // return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+
+  // Checks to see whether February has 29 days that year
+  return new Date(year, 1, 29).getDate() === 29;
 }
