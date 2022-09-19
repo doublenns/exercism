11e646e3d088fa559fb4bfa25bc8a8a2c16634f3
DoubleNNs <nnamdianyanwu@gmail.com>
2022-09-19 17:13:54 -0500

    Helper function for checking if an index is OoB of a given slice

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -8,7 +8,7 @@ func FavoriteCards() []int {
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	if index >= len(slice) || index < 0 {
+	if outOfBounds(slice, index) {
 		return -1
 	}
 	return slice[index]
@@ -17,7 +17,7 @@ func GetItem(slice []int, index int) int {
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	if index >= len(slice) || index < 0 {
+	if outOfBounds(slice, index) {
 		slice = append(slice, value)
 	} else {
 		slice[index] = value
@@ -32,8 +32,12 @@ func PrependItems(slice []int, values ...int) []int {
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	if index >= len(slice) || index < 0 {
+	if outOfBounds(slice, index) {
 		return slice
 	}
 	return append(slice[:index], slice[index+1:]...)
+}
+
+func outOfBounds(s []int, i int) bool {
+	return i >= len(s) || i < 0
 }
