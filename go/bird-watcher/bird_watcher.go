8caa1edb57304ba78a306e261ad34a1592d02f73
DoubleNNs <nnamdianyanwu@gmail.com>
2022-09-21 02:19:22 -0500

    [For] Loops

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,28 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	var total int
+	for _, birds := range birdsPerDay {
+		total += birds
+	}
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	start := (week * 7) - 7
+	birdsPerWeek := birdsPerDay[start : start+7]
+	return TotalBirdCount(birdsPerWeek)
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i]++
+	}
+	return birdsPerDay
+}
