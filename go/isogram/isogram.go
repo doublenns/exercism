336ab261c22b3d3f428758fe106d6b4f36d6c4b2
DoubleNNs <nnamdianyanwu@gmail.com>
2022-09-21 02:59:24 -0500

    Empty structs take up less mem than bools

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -7,14 +7,14 @@ import (
 
 func IsIsogram(word string) bool {
 	word = strings.ToLower(word)
-	seen := make(map[rune]bool)
+	seen := make(map[rune]struct{})
 
 	for _, r := range word {
 		if unicode.IsLetter(r) {
 			if _, ok := seen[r]; ok {
 				return false
 			} else {
-				seen[r] = true
+				seen[r] = struct{}{}
 			}
 		}
 	}
