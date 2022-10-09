ac1fe28238b74cda4bb95a81ea03fe8aae0deb74
DoubleNNs <nnamdianyanwu@gmail.com>
2022-10-09 16:30:10 -0500

    Practice using maps

---
+++ go/gross-store/gross_store.go
@@ -0,0 +1,53 @@
+package gross
+
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
+}
+
+// NewBill creates a new bill.
+func NewBill() map[string]int {
+	return map[string]int{}
+}
+
+// AddItem adds an item to customer bill.
+func AddItem(bill, units map[string]int, item, unit string) bool {
+	if num, ok := units[unit]; ok {
+		bill[item] += num
+		return true
+	}
+	return false
+}
+
+// RemoveItem removes an item from customer bill.
+func RemoveItem(bill, units map[string]int, item, unit string) bool {
+	if num, ok := units[unit]; ok {
+		if quantity, ok := bill[item]; ok {
+			if quantity < num {
+				return false
+			} else if quantity == num {
+				delete(bill, item)
+				return true
+			} else {
+				bill[item] -= num
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// GetItem returns the quantity of an item that the customer has in his/her bill.
+func GetItem(bill map[string]int, item string) (int, bool) {
+	if quantity, ok := bill[item]; ok {
+		return quantity, true
+	}
+	return 0, false
+}
