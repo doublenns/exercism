401789737a1af0084609bd2a583e995bf04ae210
DoubleNNs <nnamdianyanwu@gmail.com>
2022-10-26 16:19:21 -0500

    Rename result var

--- go/chessboard/chessboard.go
+++ go/chessboard/chessboard.go
@@ -9,49 +9,49 @@ type Chessboard map[string]File
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file.
 func CountInFile(cb Chessboard, file string) int {
-	var result int
+	count := 0
 	for _, occupied := range cb[file] {
 		if occupied {
-			result++
+			count++
 		}
 	}
-	return result
+	return count
 }
 
 // CountInRank returns how many squares are occupied in the chessboard,
 // within the given rank.
 func CountInRank(cb Chessboard, rank int) int {
+	count := 0
 	if rank > 8 || rank < 1 {
-		return 0
+		return count
 	}
 
-	var result int
 	for _, v := range cb {
 		if v[rank-1] {
-			result++
+			count++
 		}
 	}
-	return result
+	return count
 }
 
 // CountAll should count how many squares are present in the chessboard.
 func CountAll(cb Chessboard) int {
-	var result int
+	count := 0
 	for _, v := range cb {
-		result += len(v)
+		count += len(v)
 	}
-	return result
+	return count
 }
 
 // CountOccupied returns how many squares are occupied in the chessboard.
 func CountOccupied(cb Chessboard) int {
-	var result int
+	count := 0
 	for _, v := range cb {
 		for _, occupied := range v {
 			if occupied {
-				result++
+				count++
 			}
 		}
 	}
-	return result
+	return count
 }
