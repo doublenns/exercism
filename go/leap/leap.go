dc801cf127b9a139037d4b3a47f05ac8e1120163
DoubleNNs <nnamdianyanwu@gmail.com>
2022-11-04 21:06:13 -0500

    Rephrase doc comment

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,7 +1,7 @@
 // Package leap reports whether a given year is a leap year.
 package leap
 
-// IsLeapYear returns whether a given year is a leap year.
+// IsLeapYear returns a boolean indiciating whether the given year is a leap year or not.
 func IsLeapYear(y int) bool {
 	if y%100 == 0 {
 		return y%400 == 0
