d567b1ddc8a3934eabfa6dadf233ab54b7ee6e2a
Nnamdi Anyanwu <nnamdi.anyanwu@propense.ai>
2023-12-19 09:34:10 -0600

    surface-level refactoring for readability + conciseness

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -16,7 +16,7 @@ func Schedule(date string) time.Time {
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
 	t, _ := time.Parse("January 2, 2006 15:04:05", date)
-	return time.Now().After(t)
+	return t.Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
@@ -28,13 +28,11 @@ func IsAfternoonAppointment(date string) bool {
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
 	t, _ := time.Parse("1/2/2006 15:04:05", date)
-	return fmt.Sprintf("You have an appointment on %s.",
-		t.Format("Monday, January 2, 2006, at 15:04"))
+	return fmt.Sprintf("You have an appointment on %s.", t.Format("Monday, January 2, 2006, at 15:04"))
 }
 
 // AnniversaryDate returns a Time with this year's anniversary.
 func AnniversaryDate() time.Time {
-	date := fmt.Sprintf("%d-09-15", time.Now().Year())
-	t, _ := time.Parse("2006-01-02", date)
+	t, _ := time.Parse("2006-01-02", fmt.Sprintf("%d-09-15", time.Now().Year()))
 	return t
 }
